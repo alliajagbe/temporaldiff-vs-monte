9b0a7189af8075cbb9ad5c6366ea18a13203d476
alliajagbe <alliajagbe001@gmail.com>
2023-02-06 22:41:42 +0530

    modified the terminal conditions

--- qlearning.py
+++ qlearning.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Define the environment
 n_states = 6
-n_actions = 6
+n_actions = 4
 P = np.zeros((n_states, n_actions, n_states))
 R = np.zeros((n_states, n_actions, n_states))
 terminal_states = [0, n_states - 1]
@@ -15,7 +15,8 @@ epsilon = 0.1
 Q = np.zeros((n_states, n_actions))
 
 for episode in range(n_episodes):
-    state = np.random.choice([i for i in range(n_states) if i not in terminal_states])
+    non_terminal_states = [i for i in range(n_states) if i not in terminal_states]
+    state = np.random.choice(non_terminal_states)
     while state not in terminal_states:
         if np.random.uniform(0, 1) < epsilon:
             action = np.random.choice(n_actions)
@@ -33,7 +34,8 @@ for episode in range(n_episodes):
         state = next_state
 
 # Use the learned Q-table to make decisions
-state = np.random.choice([i for i in range(n_states) if i not in terminal_states])
+non_terminal_states = [i for i in range(n_states) if i not in terminal_states]
+state = np.random.choice(non_terminal_states)
 while state not in terminal_states:
     action = np.argmax(Q[state, :])
     next_state_probs = P[state, action, :]
@@ -43,7 +45,7 @@ while state not in terminal_states:
     else:
         next_state_probs = next_state_probs / prob_sum
     next_state = np.random.choice(n_states, p=next_state_probs)
-    print("From state {} take action {} to reach state {}".format(state, action, next_state))
+    print(f"From state {state}, take action {action} to reach state {next_state}.")
     state = next_state
 
 
@@ -54,5 +56,5 @@ import matplotlib.pyplot as plt
 plt.imshow(Q, cmap='hot', interpolation='nearest')
 plt.xlabel('Action')
 plt.ylabel('State')
-plt.title('Q-table')
+plt.title('Q-table for the States and Respective Actions')
 plt.show()
