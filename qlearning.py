bca868856737e2f3a5c04f5d879e4ce8d21af375
alliajagbe <alliajagbe001@gmail.com>
2023-02-08 08:55:12 +0530

    redefined the parameters

--- qlearning.py
+++ qlearning.py
@@ -1,35 +1,35 @@
 import numpy as np
 
 # defining the constant parameters
-n_states = 6
+states = 6
 actions = 4
-n_episodes = 1000
+episodes = 1000
 alpha = 0.1
 gamma = 0.99
 epsilon = 0.1
 
 # initializing the required parameters
-P = np.zeros((n_states, actions, n_states))
-R = np.zeros((n_states, actions, n_states))
-Q = np.zeros((n_states, actions))
+P = np.zeros((states, actions, states))
+R = np.zeros((states, actions, states))
+Q = np.zeros((states, actions))
 
 # setting the terminal states
 # state 0 as the "pit"
-# and state (n_states - 1) as "the goal"
-terminal_states = [0, n_states - 1]
+# and state (states - 1) as "the goal"
+terminal_states = [0, states - 1]
 
 # setting our reward values
 # 100 if it gets to the goal
 # -100 if it slips into the pit
-R[:,:,n_states-1] = 100
+R[:,:,states-1] = 100
 R[0:,:] = -100
 
 
 # running the algorithm for all episodes
-for episode in range(n_episodes):
+for episode in range(episodes):
 
     # identifying all non-terminal states
-    non_terminal_states = [i for i in range(n_states) if i not in terminal_states]
+    non_terminal_states = [i for i in range(states) if i not in terminal_states]
 
     # because we are just starting, we choose a state randomly to explore
     state = np.random.choice(non_terminal_states)
@@ -47,12 +47,12 @@ for episode in range(n_episodes):
         next_state_probs = P[state, action, :]
         prob_sum = np.sum(next_state_probs)
         if prob_sum == 0:
-            next_state_probs = np.ones(n_states) / n_states
+            next_state_probs = np.ones(states) / states
         else:
             next_state_probs = next_state_probs / prob_sum
 
 
-        next_state = np.random.choice(n_states, p=next_state_probs)
+        next_state = np.random.choice(states, p=next_state_probs)
 
         # getting the reward for each state, action, and next_state trio
         reward = R[state, action, next_state]
@@ -87,7 +87,7 @@ while state not in terminal_states:
 
     print(f"From your current state {state},")
     print(f"Take action {action} to go to the next state {next_state}.")
-    print("\nYou're welcome :)")
+    print("\nYou're welcome :)\n")
     state = next_state
 
 
