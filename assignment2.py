8e6d0af84087aab64b466cb7262cc0be2cffb5d1
alliajagbe <alliajagbe001@gmail.com>
2023-03-04 00:31:54 +0530

    visualizing sarsa

--- assignment2.py
+++ assignment2.py
@@ -91,3 +91,6 @@ def qlearning_cliffwalk(alpha, gamma, epsilon, num_episodes):
 
 q_sarsa = sarsa_cliffwalk(0.5, 1, 0.1, 500)
 q_qlearning = qlearning_cliffwalk(0.5, 1, 0.1, 500)
+
+plt.imshow(q_sarsa , cmap = 'autumn' , interpolation = 'nearest' )
+plt.show()
